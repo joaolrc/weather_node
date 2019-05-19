84fe3eca78b889ecf37315b7177eff435c9b395c
João Costa <38216435+joaolrc@users.noreply.github.com>
2019-05-19 20:26:32 +0100

    updated server.js to order entries

--- server.js
+++ server.js
@@ -21,15 +21,17 @@ app.get('/', function (req, res) {
 
   app.post('/search', function (req, res) {
 
-    let city1 = req.body.city1;
+    let city1 = req.body.city1.trim().toLowerCase();
     let url1= `http://api.openweathermap.org/data/2.5/weather?q=${city1}&units=metric&appid=${apiKey}`;
-    let city2 = req.body.city2;
+    let city2 = req.body.city2.trim().toLowerCase();
     let url2 = `http://api.openweathermap.org/data/2.5/weather?q=${city2}&units=metric&appid=${apiKey}`;
-    let city3 = req.body.city3;
+    let city3 = req.body.city3.trim().toLowerCase();
     let url3 = `http://api.openweathermap.org/data/2.5/weather?q=${city3}&units=metric&appid=${apiKey}`;
     var urls=[url1,url2,url3];
+    var allcity=[city1,city2,city3]
+    console.log(allcity);
 
-    console.log(city1+city2+city3);
+    //console.log(city1+city2+city3);
     var counter=0;
     var count_catch=0;
     var req_data=['','','']; //array vai ser preenchido com 3 respostas
@@ -40,42 +42,53 @@ app.get('/', function (req, res) {
     var dateset =[];
     var city = [];
     var badnames = 0; //flag to triger error alert deactivated
-    var lastPart=['','',''];
+    var cods =[];
+
 
     async.each(urls, function(element, done) {
-      console.log(element);
+      //console.log(element);
       request(element,  //elemento do array
         function (e, r, body) {
-          req_data[counter] = JSON.parse(body);
+          req_data[counter] = JSON.parse(body); //parse response
           try{
             console.log(req_data[counter].cod);
             if(req_data[counter].cod=='200'){
-              temp[counter]=req_data[counter].main.temp; //array com temperaturas
-              sunrise[counter]=req_data[counter].sys.sunrise; //array com amanhecer
-              sunset[counter]=req_data[counter].sys.sunset;  //array com por do sol
-              daterise[counter]=new Date(sunrise[counter]*1000);
-              daterise[counter]=daterise[counter].getHours() + ":" + daterise[counter].getMinutes() + ' ' + daterise[counter].toString().substr(34,40);
-              dateset[counter]=new Date(sunset[counter]*1000);
-              lastPart[counter] = dateset[counter].toString().substr(34,40);
-              dateset[counter]=dateset[counter].getHours() + ":" + dateset[counter].getMinutes() + ' ' + dateset[counter].toString().substr(34,40);
-              city[counter]=req_data[counter].name;  //array com por do sol
+              let counteach=0; //reset counter foreach loop
+              allcity.forEach(function(entry) { //cycle to order responses like the submited request by the user
+
+                if(req_data[counter].name.trim().toLowerCase()==entry){ //remove first character because of capital letter
+                  temp[counteach]=req_data[counter].main.temp; //array com temperaturas
+                  sunrise[counteach]=req_data[counter].sys.sunrise; //array com amanhecer
+                  sunset[counteach]=req_data[counter].sys.sunset;  //array com por do sol
+                  daterise[counteach]=new Date(sunrise[counteach]*1000);
+                  daterise[counteach]=daterise[counteach].getHours() + ":" + daterise[counteach].getMinutes() + ' ' + daterise[counteach].toString().substr(34,40);
+                  dateset[counteach]=new Date(sunset[counter]*1000);
+                  dateset[counteach]=dateset[counteach].getHours() + ":" + dateset[counteach].getMinutes() + ' ' + dateset[counteach].toString().substr(34,40);
+                  city[counteach]=entry;  //array com por do sol
+                  cods[counteach]=req_data[counter].cod;
+                }
+                counteach++;
+              });
               counter++;
               done();
               if(counter==3){ //corre 1 vez no fim do ultimo request
-                logger.info(req_data[counter].cod+' ; '+city1+' , '+city2+' , '+city3); //log message
+                console.log(city);
+                logger.info(cods[2]+' ; '+city1+' , '+city2+' , '+city3); //log message with last response code
                 res.render('index', {badnames:badnames, error: null, temp0: temp[0], temp1: temp[1], temp2: temp[2], city0:city[0], city1:city[1], city2:city[2],
                   sunrise0:sunrise[0], sunrise1:sunrise[1], sunrise2:sunrise[2], sunset0:sunset[0], sunset1:sunset[1], sunset2:sunset[2],
                   daterise0:daterise[0], daterise1:daterise[1], daterise2:daterise[2],dateset0:dateset[0], dateset1:dateset[1],  dateset2:dateset[2] });
                 };
               }else{
+                //console.log('else1'); //debug msg
                 counter++;
                 if(counter==3){
-                  logger.info(req_data[counter].cod+' ; '+city1+' , '+city2+' , '+city3); //log message
+                  logger.info(JSON.parse(body).cod+' ; '+city1+' , '+city2+' , '+city3); //log message with last response code
                   badnames=1;
                   res.render('index', {badnames:badnames, error: null, temp0: null, temp1: null, temp2: null, city0:null, city1:null, city2:null,
                     sunrise0:null, sunrise1:null, sunrise2:null, sunset0:null, sunset1:null, sunset2:null,
                     daterise0:null, daterise1:null, daterise2:null,dateset0:null, dateset1:null,  dateset2:null });
                   }}}catch(err){
+                    console.log(err);
                     // if(counter==2){ //corre 1 vez no fim do ultimo request
                     badnames=1; //flag to triger error alert activated
                     res.render('index', {badnames:badnames, error: null, temp0: null, temp1: null, temp2: null, city0:null, city1:null, city2:null,
